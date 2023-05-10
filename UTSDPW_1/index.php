023eede2ebfc574f6129da2a3dc7226735258dba
BintangSafna333 <113089950+BintangSafna333@users.noreply.github.com>
2023-05-10 10:13:28 +0700

    Add files via upload
    UTS Bismillah

---
+++ UTSDPW_1/index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>21104067_BintangAbillahSafna_UTSDPW1</title>
+</head>
+<body>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.7/dist/umd/popper.min.js" integrity="sha384-zYPOMqeu1DAVkHiLqWBUTcbYfZ8osu1Nd6Z89ify25QV9guujx43ITvfi12/QExE" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.min.js" integrity="sha384-Y4oOpwW3duJdCWv5ly8SCFYWqFDsfob/3GkgExXKV4idmbt98QcxXYs9UoXAB7BZ" crossorigin="anonymous"></script>
+
+    <header>
+        <nav class="bg-body-tertiary">
+            <div class="container text-center">
+                <div class="col-12">
+                <p>Home Page</p>
+                </div>
+            </div>
+            <div class="container-fluid">
+                <ul class="nav">
+                <li class="d-flex; nav-item">
+                    <a class="nav-link" href="#">
+                        <img src="#" alt="left">
+                    </a>
+                </li>
+                <li class="d-flex; nav-item">
+                    <a class="nav-link" href="#">
+                        <img src="#" alt="right">
+                    </a>
+                </li>
+                <li class="d-flex; nav-item">
+                    <a class="nav-link" href="#">
+                        <img src="#" alt="close">
+                    </a>
+                </li>
+                <li class="d-flex; nav-item">
+                    <a class="nav-link" href="#">
+                        <img src="#" alt="home">
+                    </a>
+                </li>
+                <li class="col-8">
+                <form role="search">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                </form>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <div class="container text-center" style="margin-top: 25px;">
+            <div class="row justify-content-start">
+            <div class="col">
+                <p>
+                <img src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAHwAAAB8CAMAAACcwCSMAAAAQlBMVEX///8AAADU1NTj4+OXl5eoqKgkJCQQEBDt7e2SkpLFxcV6enpCQkIKCgqBgYH5+fk8PDy0tLRZWVnOzs6Hh4egoKB3EBnLAAABmUlEQVRoge3by5KCMBAF0I6G8Iog4vz/r47oOHYIWjNVdO7m3p1sDnniolskS9P5Poba7ZY6xN53TS5lchVP+7E6p1h99o9DsJEfCcPxLT36Hed6O7Uf3wz7bE0vOW8OfipBL5lyuy1lO9eu7Ws527krbNxLkrEXW+9n1LofS9vO/e75MTtjl3aaD7tlntrLWjg/z7tPnwf/h2v4v2n86u70P5Oe3mu9AX3n+4SpHxM/JA8rG3pJlUDD/Y2S+di4fvZLcqhCs34fw3HLlhX1etvaInrd423W1X+HYLTXXtFrfGqky7e/ZfSx7uRL/TIf+G3oyVjVKlzsbRF11/V6v2XfWYuo72cUtQNMz/gz6qwHUXfrXAKfX14tagMcSuAHBRInTpw4ceLEiRMnTpw4ceLEiRMnTpw4ceLEiRMnTpw4ceLEiRMnTtwah5YnQQuzoCVp0GI8XZRYvAwRWoAJLT2FFt1iy42hhdbQEnNscT20rQDbUAFtJcE20WDbh7CNU9iWMWyzHLZNENsgKdDW0CXAptiHX6wd+BuqPx2yy61FcQAAAABJRU5ErkJggg==" height="20px" width="20px">
+                Restauranty
+                </p>
+            </div>
+            <div class="col-10">
+            <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
+                <label class="btn btn-outline-primary" for="btnradio1">Apple</label>
+
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
+                <label class="btn btn-outline-secondary" for="btnradio2">Banana</label>
+
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
+                <label class="btn btn-outline-secondary" for="btnradio3">Durian</label>
+
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off">
+                <label class="btn btn-outline-secondary" for="btnradio4">Durian</label>
+
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off">
+                <label class="btn btn-outline-secondary" for="btnradio5">Guava</label>
+
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio6" autocomplete="off">
+                <label class="btn btn-outline-secondary" for="btnradio6">Peach</label>
+
+                <input type="radio" class="btn-check" name="btnradio" id="btnradio7" autocomplete="off">
+                <label class="btn btn-outline-secondary" for="btnradio7">Orange</label>
+            </div>
+                <button type="button" class="btn btn-dark" style="margin-left: 15px;">Contact Now</button>           
+            </div>
+            </div>
+            
+            <h1 style="margin-top: 50px;">Organic Foods Made</h1>
+            <h1>Easy & Healthy</h1>
+
+            <div class="container text-center">
+                <div class="row align-items-center">
+                    <div class="col-4"></div>
+                    <div class="col" style="margin-top:20px;">
+                    <form class="d-flex" role="search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="border-radius: 20px;">
+                        <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                    </div>
+                    <div class="col"></div>
+                </div>
+            
+            </div>
+            <?php
+			// Data dari database
+			$data = array(
+				array("https://example.com/image1.jpg", "Time Food"),
+				array("https://example.com/image1.jpg", "Nature"),
+				array("https://example.com/image1.jpg", "Home Sevices"),
+				array("https://example.com/image1.jpg", "Free Delivery")
+			);
+
+			// Menampilkan data
+			foreach ($data as $d) {
+				echo '<div class="container text-center">';
+                
+				echo '<img src="' . $d[0] . '">';
+				echo '<p>' . $d[1] . '</p>';
+				echo '</div>';
+			}
+		?>
+
+        <h1 style="margin-top: 200px;">Choose & Enjoy</h1>
+        <p>Can yo get your choose in here</p>
+
+        </div>
+        <?php
+			// Data dari database
+			$data2 = array(
+				array("Judul 1", "Deskripsi 1", "https://example.com/image1.jpg"),
+				array("Judul 2", "Deskripsi 2", "https://example.com/image2.jpg"),
+				array("Judul 3", "Deskripsi 3", "https://example.com/image3.jpg"),
+				array("Judul 4", "Deskripsi 4", "https://example.com/image4.jpg"),
+				array("Judul 5", "Deskripsi 5", "https://example.com/image5.jpg")
+			);
+
+			// Menampilkan data
+			foreach ($data2 as $e) {
+                echo '<div class= "container text-center">';
+                echo '<div class= "container-fluid">';
+                echo '<div class= "col">';
+                echo '<div class="card" style="width: 18rem;">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">'; 
+				echo '<img src="' . $e[2] . '">';
+				echo '<h2>' . $e[0] . '</h2>';
+				echo '<p>' . $e[1] . '</p>';
+                echo '<a href="#" class="btn btn-primary">Go somewhere</a>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+                echo '</div>';
+				echo '</div>';
+			}
+		?>
+    </main>
+    <footer>
+        <div class="container-fluid" style="background-color: blanchedalmond;">
+        <p>&copy; 2023 All Right Reserved</p>
+        </div>
+    </footer>
+</body>
+</html>
